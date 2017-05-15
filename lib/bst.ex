8f521def95e9af13b82d2a33061c57288931f145
banzay <yuriy.zdyrko@gmail.com>
2017-05-15 11:24:37 +0300

    bst - find, remove

--- lib/bst.ex
+++ lib/bst.ex
@@ -3,8 +3,11 @@ defmodule Bst do
   import IEx
 
   def run do
-    bst = from_array(Enum.into(200..222, []))
-    print_graph(bst)
+    bst = from_array(Enum.into(200..230, []))
+    # print_graph(bst)
+    # has_value(bst, 290)
+    print_graph(insert(bst, 240))
+
   end
 
 
@@ -16,7 +19,7 @@ defmodule Bst do
     %{left: nil, right: nil, v: v}
   end
   def from_array(arr) do
-    middle_i = round(length(arr) / 2)
+    middle_i = round(length(arr) / 2 - 1)
     {left, [h|right]} = Enum.split(arr, middle_i)
     %{left: from_array(left), right: from_array(right), v: h}
   end
@@ -50,6 +53,33 @@ defmodule Bst do
     end
   end
 
+
+  def insert(nil, new_value) do
+    %{v: new_value, right: nil, left: nil}
+  end
+  def insert(bst, new_value) do
+    if !has_value(bst, new_value) do
+      if (bst.v < new_value) do
+        %{v: bst.v, right: insert(bst.right, new_value), left: bst.left}
+      else
+        %{v: bst.v, right: bst.right, left: insert(bst.left, new_value)}
+      end
+    else
+      bst
+    end
+  end
+
+  def has_value(nil, _) do
+    false
+  end
+  def has_value(bst, search_value) do
+    case bst.v do
+      v when v == search_value -> true
+      v when v < search_value -> has_value(bst.right, search_value)
+      v when v > search_value -> has_value(bst.left, search_value)
+    end
+  end
+
   def replace_nil_with_empty_array(list) do
     Enum.filter(list, fn(i) -> i !== nil end)
   end
@@ -58,6 +88,6 @@ defmodule Bst do
     "(leaf)"
   end
   def node_lvl_txt(lvl) do
-    "#{lvl.v} > (#{lvl.left && lvl.left.v} #{lvl.right && lvl.right.v})"
+    "#{lvl.v} > (#{lvl.left && lvl.left.v || "x"} #{lvl.right && lvl.right.v || "x"})"
   end
 end
